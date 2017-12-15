fe7108640f3814b38b4b353010aa76087639957c
Ba <mandiayba@gmail.com>
2017-12-16 00:47:13 +0100

    modifying start

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 /etc/init.d/postgresql start
 
 /usr/local/glassfish4/bin/asadmin start-domain
-/usr/local/glassfish4/bin/asadmin -u admin deploy /$AAE_NAME.war
+
+/usr/local/glassfish4/bin/asadmin -u admin deploy --contextroot /alvisae/$AAE_NAME/AlvisAE --name alvisae-ui /AlvisAEGenericUI.war
+
+/usr/local/glassfish4/bin/asadmin -u admin deploy --contextroot /alvisae/$AAE_NAME --name alvisae-ws /$AAE_NAME.war
+
 /usr/local/glassfish4/bin/asadmin set-web-context-param --name configFilePath --value '/glassfish.localhost.props' $AAE_NAME
 /usr/local/glassfish4/bin/asadmin stop-domain
 /usr/local/glassfish4/bin/asadmin start-domain --verbose
